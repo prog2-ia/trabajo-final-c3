3ced3152a3071b357b652f9d8d2d3195d9356ca7
lvp38 <lvp38@alu.ua.es>
2026-02-20 13:39:04 +0100

    clases y clase trabajador

---
+++ clases.py
@@ -0,0 +1,41 @@
+class Vehiculo:
+    def __init__(self,matricula,marca,modelo,color,deposito,tipo):
+        self.matricula=matricula
+        self.marca=marca
+        self.modelo=modelo
+        self.color=color
+        self.km_recorridos=0
+        self.gasolina=0
+        self.deposito=deposito
+        self.tipo=tipo
+
+    def suma_km(self,km):
+        self.km_recorridos+=km
+
+
+    def hechar_gasolina(self,gasolina):
+        if self.gasolina+gasolina<=self.deposito:
+            self.gasolina+=gasolina
+        else:
+            print("No se puede hechar gasolina por encima del limite")
+            self.gasolina=self.deposito
+
+
+class Persona
+    def __init__(self,DNI,nombre,apellido,coche):
+        self.dni=DNI
+        self.nombre=nombre
+        self.apellido=apellido
+        self.coche=coche
+
+class Trabajador
+    def __init__(self,dni,nombre,apellidos,cargo,sueldo):
+        self.dni=dni
+        self.apellidos=apellidos
+        self.cargo=cargo
+        self.sueldo=sueldo
+        self.ventas=0
+
+    def aumento(self,dinero):
+        self.sueldo+=dinero
+        print('El sueldo de ',self,' ha aumentado a ',self.sueldo,'€.')
