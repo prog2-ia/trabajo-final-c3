48eb2a30341f7f7bc50e69eb7b5dde97eaa77bb6
sfs46 <samulferrandezsanchez@gmail.com>
2026-02-20 12:36:27 +0000

    'clase coche y metodos'

--- coche.py
+++ coche.py
@@ -1,11 +1,20 @@
-class Coche:
-    def __init__(self,matricula,marca,modelo,color):
+class Vehiculo:
+    def __init__(self,matricula,marca,modelo,color,deposito,tipo):
         self.matricula=matricula
         self.marca=marca
         self.modelo=modelo
         self.color=color
         self.km_recorridos=0
         self.gasolina=0
+        self.deposito=deposito
+        self.tipo=tipo
 
     def suma_km(self,km):
-        self
+        self.km_recorridos+=km
+
+    def hechar_gasolina(self,gasolina):
+        if self.gasolina+gasolina<=self.deposito:
+            self.gasolina+=gasolina
+        else:
+            print("No se puede hechar gasolina por encima del limite")
+            self.gasolina=self.deposito
