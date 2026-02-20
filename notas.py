e5fe9f01e4886989f1cbdf3d74f6a2e4c11bddae
lvp38 <lvp38@alu.ua.es>
2026-02-20 14:06:25 +0100

    main

--- notas.py
+++
@@ -1,9 +0,0 @@
-def calcular_media(notas):
-    return sum(notas) / len(notas)
-
-def mostrar_resumen(notas):
-    media = round(calcular_media(notas),2)
-    print(f"Número de notas: {len(notas)}")
-    print(f"Nota máxima: {max(notas)}")
-    print(f"Nota mínima: {min(notas)}")
-    print(f"Nota media: {media}")
--- persona.py
+++
@@ -1,6 +0,0 @@
-class Persona
-    def __init__(self,DNI,nombre,apellido,coche):
-        self.dni=DNI
-        self.nombre=nombre
-        self.apellido=apellido
-        self.coche=coche
