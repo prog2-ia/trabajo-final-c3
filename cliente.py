e02bbe24bab0035df4c7132d5f3d36f923f858fe
lvp38 <lvp38@alu.ua.es>
2026-02-27 13:44:58 +0100

    Herencia con persona trabajador y cliente 2

--- cliente.py
+++ cliente.py
@@ -1,4 +1,6 @@
-class Cliente:
+from persona import Persona
+
+class Cliente(Persona):
     def __init__(self,dni,nombre,apellidos):
         super().__init__(dni,nombre,apellidos)
         self.vehiculos=[]
