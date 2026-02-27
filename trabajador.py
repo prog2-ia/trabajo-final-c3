4aa83549422ca2ba3cbb73d786b7429a44c8418c
lvp38 <lvp38@alu.ua.es>
2026-02-27 14:33:37 +0100

    Clases de trabajadores con herencia

--- trabajador.py
+++ trabajador.py
@@ -1,17 +1,12 @@
 from persona import Persona
 
 class Trabajador(Persona):
-    def __init__(self,dni,nombre,apellidos,cargo,sueldo_hora):
+    def __init__(self,dni,nombre,apellidos,sueldo):
         super().__init__(dni,nombre,apellidos)
         self.cargo=cargo
-        self.sueldo_hora=sueldo_hora
+        self.sueldo=sueldo
         self.ventas=0
 
     def aumento(self,dinero):
-        self.sueldo_hora+=dinero
-        print('El sueldo por hora de ',self,' ha aumentado a ',self.sueldo,'€.')
-
-    def sueldo_semanal(self,horas):
-        seuldo_s=horas*self.sueldo_hora
-        return seuldo_s
-
+        self.sueldo+=dinero
+        print('El sueldo de ',self,' ha aumentado a ',self.sueldo,'€.')
