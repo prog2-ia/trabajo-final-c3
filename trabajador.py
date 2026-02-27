03a3def8385ff9cd93091e727a44b784e6013cb4
sfs46 <samulferrandezsanchez@gmail.com>
2026-02-27 14:31:01 +0100

    metodo en Trabajador seuldo semanal

--- trabajador.py
+++ trabajador.py
@@ -1,12 +1,16 @@
 from persona import Persona
 
 class Trabajador(Persona):
-    def __init__(self,dni,nombre,apellidos,cargo,sueldo):
+    def __init__(self,dni,nombre,apellidos,cargo,sueldo_hora):
         super().__init__(dni,nombre,apellidos)
         self.cargo=cargo
-        self.sueldo=sueldo
+        self.sueldo_hora=sueldo_hora
         self.ventas=0
 
     def aumento(self,dinero):
-        self.sueldo+=dinero
-        print('El sueldo de ',self,' ha aumentado a ',self.sueldo,'€.')
+        self.sueldo_hora+=dinero
+        print('El sueldo por hora de ',self,' ha aumentado a ',self.sueldo,'€.')
+
+    def sueldo_semanal(self,horas):
+        seuldo_s=horas*self.sueldo_hora
+        return seuldo_s
