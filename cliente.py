70039c08dc02adef2b3ed25530694753f57c7fc1
sfs46 <samulferrandezsanchez@gmail.com>
2026-03-05 21:11:09 +0100

    El cliente tiene que tener mas 18,

--- cliente.py
+++ cliente.py
@@ -1,6 +1,11 @@
 from persona import Persona
 
 class Cliente(Persona):
-    def __init__(self,dni,nombre,apellidos):
-        super().__init__(dni,nombre,apellidos)
-        self.vehiculos=[]
+    def __init__(self,dni,nombre,apellidos,edad,carnet):
+        if edad>=18:
+            super().__init__(dni,nombre,apellidos)
+            self.vehiculos=[]
+            self.edad=edad
+            self.carnet=carnet
+        else:
+            print('Edad insuficiente para poder alquilar un vehiculo')
