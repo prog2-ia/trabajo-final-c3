948f521bf68c9a047d259f722e8d82e76bded719
sfs46 <samulferrandezsanchez@gmail.com>
2026-03-06 14:15:29 +0100

    clase furgonte y que actualize la tarifa segun sus dimenciones

---
+++ furgoneta.py
@@ -0,0 +1,24 @@
+from vehiculo import Vehiculo
+
+
+class Furgoneta(Vehiculo):
+    def __init__(self,matricula,marca,modelo,color,deposito,precio_d,capacidad_carga,tamaño):
+
+        super().__init__(matricula,marca,modelo,color,deposito,precio_d)
+        self.capacidad_carga=capacidad_carga
+        self.tamaño=tamaño
+
+    def actualizar_tarifa(self):
+        carga={800:1,1000:1.1,1200:1.2}
+        self.precio_d*=carga[self.capacidad_carga]
+
+        tamaños={'Pequeña':0,'Mediana':10,'Grande':20}
+
+        if self.tamaño in tamaños:
+            self.precio_d+=tamaños[self.tamaño]
+            return True
+        else:
+            print('Dimension no valida')
+            return False
+
+
