5309d5ca1a606e39cf2c9e213b61eaa82f927af7
sfs46 <samulferrandezsanchez@gmail.com>
2026-03-13 14:32:58 +0100

    Servicio GestionSEde

---
+++ GestionSede.py
@@ -0,0 +1,22 @@
+from Entidades.sede import Sede
+class GestorSede:
+    def __init__(self):
+        self.sedes = []
+
+    def anadir_sede(self, sede):
+        if isinstance(sede,Sede):
+            if self.buscar_sede_por_id(sede.idSede) is not None:
+                self.sedes.append(sede)
+
+    def buscar_sede_por_id(self, id_sede):
+        for sede in self.sedes:
+            if sede.id_sede == id_sede:
+                return sede
+        return None
+
+    def buscar_sede_por_nombre(self, nombre):
+        for sede in self.sedes:
+            if sede.nombre.lower() == nombre.lower():
+                return sede
+        return None
+
