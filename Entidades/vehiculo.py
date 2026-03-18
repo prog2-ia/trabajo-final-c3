dd570c3303c7bb57a993387d59af4e5e24f8a24d
sfs46 <samulferrandezsanchez@gmail.com>
2026-03-18 12:40:03 +0100

    Atributos protegidos en Vehiculo _matricula,_modelo,_marca

--- Entidades/vehiculo.py
+++ Entidades/vehiculo.py
@@ -1,19 +1,33 @@
-#Obj sede
+# Obj sede
 class Vehiculo:
-    def __init__(self,matricula,marca,modelo,color,deposito,tipo,consumo,precio_d):
-        self.matricula=matricula
-        self.precio_d=precio_d
-        self.marca=marca
-        self.modelo=modelo
-        self.color=color
-        self.km_recorridos=0
-        self.gasolina=0
-        self.deposito=deposito
-        self.tipo=tipo
-        self.averías=[]
-        self.resevas=[]
-        self.consumo=consumo
-        self.ocupado=False
+    def __init__(self, matricula, marca, modelo, color, deposito, tipo, consumo, precio_d):
+
+        self._matricula = matricula
+        self._marca = marca
+        self._modelo = modelo
+
+        self.precio_d = precio_d
+        self.color = color
+        self.km_recorridos = 0
+        self.gasolina = 0
+        self.deposito = deposito
+        self.tipo = tipo
+        self.averias = []
+        self.reservas = []
+        self.consumo = consumo
+        self.ocupado = False
+
+    @property
+    def matricula(self):
+        return self._matricula
+
+    @property
+    def marca(self):
+        return self._marca
+
+    @property
+    def modelo(self):
+        return self._modelo
 
     def __str__(self):
         return f"{self.marca} {self.modelo} ({self.matricula}) - KM: {self.km_recorridos} - Combustible: {self.gasolina}/{self.deposito}L"
@@ -21,23 +35,20 @@ class Vehiculo:
     def __repr__(self):
         return f"Vehiculo(matricula='{self.matricula}', marca='{self.marca}', modelo='{self.modelo}')"
 
-    def suma_km(self,km):
-        self.km_recorridos+=km
+    def suma_km(self, km):
+        self.km_recorridos += km
 
-
-    def echar_gasolina(self,gasolina):
-        if self.tipo=='eléctrico':
-            print('Has intentado echar gasolina en un coche eléctrico, tu motor ha sufrido una explosión.')
-            self.averías.append('Explosion de motor por combustible')
+    def echar_gasolina(self, gasolina):
+        if self.tipo == 'eléctrico':
+            print('Has intentado echar gasolina en un coche eléctrico.')
+            self.averias.append('Explosión de motor por combustible')
         else:
-            if self.gasolina+gasolina<=self.deposito:
-                self.gasolina+=gasolina
+            if self.gasolina + gasolina <= self.deposito:
+                self.gasolina += gasolina
             else:
-                print("No se puede hechar gasolina por encima del limite")
-                self.gasolina=self.deposito
+                print("No se puede echar gasolina por encima del límite")
+                self.gasolina = self.deposito
+
     def mostrar_averias(self):
-
-        for i in self.averías:
+        for i in self.averias:
             print(i)
-
-
