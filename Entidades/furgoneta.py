7ab796645abdfb207722ef94cfb4dc3c2f5d4163
lvp38 <lvp38@alu.ua.es>
2026-04-27 13:26:02 +0200

    Sugerencia de tipos perfilada y completa en furgoneta y moto

--- Entidades/furgoneta.py
+++ Entidades/furgoneta.py
@@ -2,18 +2,18 @@ from Entidades.vehiculo import Vehiculo
 
 #
 class Furgoneta(Vehiculo):
-    def __init__(self,matricula:str,marca:str,modelo:str,color:str,deposito:int,tipo:str,consumo:float,precio_d:float,capacidad_carga:int,tamaño:str):
+    def __init__(self,matricula:str,marca:str,modelo:str,color:str,deposito:float,tipo:str,consumo:float,precio_d:float,capacidad_carga:int,tamaño:str)->None:
 
         super().__init__(matricula,marca,modelo,color,deposito,tipo,consumo,precio_d)
-        self.capacidad_carga=capacidad_carga
-        self.tamaño=tamaño
+        self.capacidad_carga: int =capacidad_carga
+        self.tamaño:str =tamaño
 
 #Función que actualiza el precio según la carga que puede llevar la furgoneta y el tamaño
     def actualizar_tarifa(self) -> bool:
-        carga={800:1,1000:1.1,1200:1.2}
+        carga: dict[int, float]={800:1,1000:1.1,1200:1.2}
         self.precio_d*=carga[self.capacidad_carga]
 
-        tamaños={'Pequeña':0,'Mediana':10,'Grande':20}
+        tamaños: dict[str, int] ={'Pequeña':0,'Mediana':10,'Grande':20}
 
         if self.tamaño in tamaños:                  #Comprueba que el tamaño exista
             self.precio_d+=tamaños[self.tamaño]
--- Entidades/moto.py
+++ Entidades/moto.py
@@ -2,9 +2,9 @@ from Entidades.vehiculo import Vehiculo
 #Hereda de vehiculo
 class Moto(Vehiculo):
     numero_motos=0   #Contador para saber cuántas motos hay
-    def __init__(self,matricula:str,marca:str,modelo:str,color:str,deposito:int,tipo:str,consumo:float,precio_d:float,cilindrada:int):
+    def __init__(self,matricula:str,marca:str,modelo:str,color:str,deposito:float,tipo:str,consumo:float,precio_d:float,cilindrada:int)-> None:
         super().__init__(matricula,marca,modelo,color,deposito,tipo,consumo,precio_d)
-        self._cilindrada=cilindrada
+        self._cilindrada: int=cilindrada
         Moto.numero_motos+=1
 
     @property
